468235016a73667e65e878f813c0648bc6209d5c
Xingguo Huang <76279571+308241561@users.noreply.github.com>
2023-04-30 18:37:11 -0700

    "Changed the displaying similar movies to now playing movie to avoid app crash."

--- Tipster/MovieGridViewController.swift
+++ Tipster/MovieGridViewController.swift
@@ -32,7 +32,9 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
 
         // Do any additional setup after loading the view.
         
-        let url = URL(string: "https://api.themoviedb.org/3/movie/634649/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        // Previously, this is to show the similar movie, but there are some null posters, so changed back to the now playing moives to avoid the app crash.
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         let task = session.dataTask(with: request) { (data, response, error) in
